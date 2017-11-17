653fb51ef01fa5ad853638f6a67ebe6f071b2cc3
Frank J. Reashore <frank.reashore@gov.bc.ca>
2017-11-17 11:54:01 -0800

    Initial commit

---
+++ buildScripts.js/srcServer.js
@@ -0,0 +1,20 @@
+
+import express from 'express';
+import path from 'path';
+import open from 'open';
+
+const port = 3000;
+let app = express();
+
+app.get('/', function(request, response) {
+  response.sendFile(path.join(__dirname, "../src/index.html'));"))
+});
+
+app.listen(port, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    open('http://localhost:' + port);
+  }
+
+})
